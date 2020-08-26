ba41d72e45eeaa816291e9a5cc9deccda8ffc887
Madavi <mran0011@student.monash.edu>
2020-08-26 14:20:21 +1000

    Initial push

---
+++ FIT5031_WK3/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FIT5031_WK3.MvcApplication" Language="C#" %>
---
+++ FIT5031_WK3/HelloWorld/Hello.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FIT5031_WK3.HelloWorld
+{
+    public class Hello
+    {
+        public String GetHello()
+        {
+            return "Hello World!";
+        }
+    }
+}
